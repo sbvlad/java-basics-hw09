97bf7ee6e79933c306abe3c11feec4d316690290
Vladyslav Kulichenko <vladyslavkulichenko@vladyslav.kulichenko>
2020-03-26 19:22:58 +0200

    Task Solved

--- src/ArrayUtil.java
+++ src/ArrayUtil.java
@@ -1,19 +1,45 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.HashSet;
+
+import java.util.List;
+import java.util.Set;
 
 /**
  * ArrayUtil exercises.
  */
 public class ArrayUtil {
 
-  /**
-   * Finds the common elements between two arrays (String values).
-   *
-   * @param array1 first array
-   * @param array2 second array
-   * @return String array with common elements
-   */
-  public static String[] findCommon(String[] array1, String[] array2) {
-    // TODO fill in code here
-    return new String[0];
-  }
+    /**
+     * Finds the common elements between two arrays (String values).
+     *
+     * @param array1 first array
+     * @param array2 second array
+     * @return String array with common elements
+     */
+    public static String[] findCommon(String[] array1, String[] array2) {
+
+        Set sortingSet = new HashSet();
+        List<String> comparingList = new ArrayList<String>();
+
+        for (String i : array2) {
+          sortingSet.add(i);
+        }
+
+
+        for (int i = 0; i < array1.length; i++) {
+
+
+            if (!sortingSet.add(array1[i])) {
+                comparingList.add(array1[i]);
+            }
+        }
+
+        String[] result = new String[comparingList.size()];
+
+        for (int i = 0; i < comparingList.size(); i++)
+            result[i] = comparingList.get(i);
+
+        return result;
+    }
 }
--- src/ArraysTest.java
+++ src/ArraysTest.java
@@ -5,27 +5,38 @@ import org.testng.annotations.Test;
 
 public class ArraysTest {
 
-  /**
-   * Test data for common array elements.
-   *
-   * @return test data array
-   */
-  @DataProvider(name = "ArrayUtil")
-  public static Object[][] arrays() {
-    return new Object[][]{
-        {
-            new String[]{"Python", "JAVA", "PHP", "C#", "C++", "SQL"},
-            new String[]{"MySQL", "SQL", "SQLite", "Oracle", "PostgreSQL", "DB2", "JAVA"},
-            new String[]{"JAVA", "SQL"}
-        }
-        // TODO add 2 more test data here
-    };
-  }
+    /**
+     * Test data for common array elements.
+     *
+     * @return test data array
+     */
+    @DataProvider(name = "ArrayUtil")
+    public static Object[][] arrays() {
+        return new Object[][]{
 
-  @Test(dataProvider = "ArrayUtil")
-  public void testFindCommon(String[] array1, String[] array2, String[] expectedResult) {
+                {
+                        new String[]{"Python", "JAVA", "PHP", "C#", "C++", "SQL"},
+                        new String[]{"MySQL", "SQL", "SQLite", "Oracle", "PostgreSQL", "DB2", "JAVA"},
+                        new String[]{"JAVA", "SQL"}
+                },
+                {
+                        new String[]{"Mom", "Dad", "Bro", "Sis", "Granny", "Aunt"},
+                        new String[]{"Stranger", "Delivery", "Policeman", "Mom", "Granny", "Firefighter", "Jedi"},
+                        new String[]{"Mom", "Granny"}
+                },
+                {
+                        new String[]{"Suzuki", "Toyota", "Jeep", "Ford", "Mazda", "Geely"},
+                        new String[]{"Mazda", "Honda", "Toyota", "Acura", "PostgreSQL", "Infinity", "Geely"},
+                        new String[]{"Toyota", "Mazda", "Geely"}
+                }
 
-    assertEquals(ArrayUtil.findCommon(array1, array2), expectedResult,
-        "Common elements are not correct");
-  }
+        };
+    }
+
+    @Test(dataProvider = "ArrayUtil")
+    public void testFindCommon(String[] array1, String[] array2, String[] expectedResult) {
+
+        assertEquals(ArrayUtil.findCommon(array1, array2), expectedResult,
+                "Common elements are not correct");
+    }
 }
